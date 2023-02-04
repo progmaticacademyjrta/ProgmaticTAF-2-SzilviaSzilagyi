787cf40531205658a6daa5ecf288bfffed7ac21b
szilviaszilagyi <kovacs.szilvia.dolli@gmail.com>
2023-02-04 17:50:10 +0100

    exam added

--- src/test/java/hu/progmatic/pages/HomePage.java
+++ src/test/java/hu/progmatic/pages/HomePage.java
@@ -3,6 +3,7 @@ package hu.progmatic.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
@@ -10,6 +11,7 @@ public class HomePage {
     WebDriver driver;
     WebDriverWait wait;
     By headerBy = By.id("top");
+    By makeAppointmentButtonBy = By.id("btn-make-appointment");
 
     public HomePage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
@@ -22,4 +24,14 @@ public class HomePage {
         Assert.assertTrue(headerElement.isDisplayed());
         System.out.println("Homepage loaded properly.");
     }
+
+    public void clickMakeAppointmentButton(){
+        WebElement makeAppointmentButton = wait.until(ExpectedConditions.elementToBeClickable(makeAppointmentButtonBy));
+        makeAppointmentButton.click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://katalon-demo-cura.herokuapp.com/profile.php#login");
+        System.out.println("The login page loaded.");
+    }
+
+
+
 }
